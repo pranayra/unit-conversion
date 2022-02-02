c8ddcca38b27a9e4a97a842474700898a99fec00
Pranay <pranay@pranays-mbp.attlocal.net>
2022-02-02 15:39:40 -0600

    unit-conversion initial commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import { convertUnits  } from "./utils/utils";
+
+function App() {
+  const [validated, setValidated] = useState(false);
+  const [result, setResult] = useState('');
+  const [inputValue, setInputValue] = useState(null);
+  const [sourceUnit, setSourceUnit] = useState('Kelvin');
+  const [targetUnit, setTargetUnit] = useState('Celsius');
+  const [studentResponse, setStudentResponse] = useState(null);
+
+  const inputValues = ["Kelvin", "Celsius", "Fahrenheit", "Rankine", "liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"]
+  
+  function valuechanges(e) {
+    setResult('')
+    const validations = {
+      inputValue: inputValue,
+      sourceUnit: sourceUnit,
+      targetUnit: targetUnit,
+      studentResponse: studentResponse,
+    }
+    if (e.target.id === "inputValue") {
+      setInputValue(e.target.value);
+      validations['inputValue'] = e.target.value;
+    }
+
+    if (e.target.id === "sourceUnit") {
+      setSourceUnit(e.target.value);
+      validations['sourceUnit'] = e.target.value;
+    }
+
+    if (e.target.id === "targetUnit") {
+      setTargetUnit(e.target.value);
+      validations['targetUnit'] = e.target.value;
+    }
+
+    if (e.target.id === "studentResponse") {
+      setStudentResponse(e.target.value);
+      validations['studentResponse'] = e.target.value;
+    }
+
+    const isValid = Object.keys(validations).filter(el => validations[el]).length === 4;
+    setValidated(isValid);
+  }
+
+  function convertValues() {
+    let convertedValue = convertUnits(sourceUnit,targetUnit,inputValue)
+    if(convertedValue==='invalid') {
+setResult('invalid');
+    } else {
+      const roundedValue = Math.round(10*convertedValue)/10; 
+      const roundedStudentResponse = Math.round(10*studentResponse)/10; 
+      console.log(convertedValue,roundedValue,roundedStudentResponse)
+      if(roundedValue===roundedStudentResponse) {
+        setResult('correct');
+      } else {
+        setResult('incorrect');
+      }
+    }
+
+  }
+
+  return (
+    <div>
+                  <div className="py-12 bg-gray-700 transition duration-150 ease-in-out z-10 absolute top-0 right-0 bottom-0 left-0" id="modal">
+                <div role="alert" className="container mx-auto w-11/12 md:w-2/3 max-w-lg">
+                    <div className="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                        <div className="w-full flex justify-start text-gray-600 mb-3">
+                        </div>
+                        <h1 className="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Enter Conversion details</h1>
+                        <label htmlFor="name" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Input Value</label>
+                        <div className="flex">
+                          <input onChange={(e)=> valuechanges(e)} id="inputValue" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Enter value here" />
+                          <select id="sourceUnit" name="Reason" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" value={sourceUnit} onChange={(e) => valuechanges(e)} >
+                  {inputValues.map((option, index) => (
+                    <option key={index} value={option}>{option}</option>
+                  ))}
+                </select>
+                        </div>
+                        <label htmlFor="name" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Target Unit</label>
+                        <div className="flex">
+                        <select id="targetUnit" name="Reason" className="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" value={targetUnit} onChange={(e) => valuechanges(e)} >
+                  {inputValues.map((option, index) => (
+                    <option key={index} value={option}>{option}</option>
+                  ))}
+                  </select>
+
+                        </div>
+                        <label htmlFor="name" className="text-gray-800 text-sm font-bold leading-tight tracking-normal">Student's Response</label>
+                        <input onChange={(e)=> valuechanges(e)} id="studentResponse" className="mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Student's Response here" />
+
+
+                        <button onClick={convertValues} disabled={!validated} className={`focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out mt-3 rounded text-white px-8 py-2 text-sm ${!validated ? 'bg-gray-400':'bg-indigo-700 hover:bg-indigo-600'}`}>Submit</button>
+                        <div>
+                        {result?
+                        <>
+                        {result==="correct" ?
+                        <>
+                        <div className="mt-5 py-2 px-2 mb-4 bg-green-100 text-green-900 text-sm rounded-md border border-green-200">
+                        {`Your answer is ${result}`}
+                        </div>
+                        </>
+                        :
+                        <>
+                        <div className="mt-4 py-2 px-2 mb-4 bg-red-100 text-red-900 text-sm rounded-md border border-red-200">
+                          {`Your answer is ${result}`}
+                        </div>
+                        </>}
+                        </>
+                      :<></>}
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+      
+    </div>  
+);
+}
+
+export default App;
---
+++ src/utils/utils.js
@@ -0,0 +1,232 @@
+function convertUnits(sourceUnit,targetUnit, sourceValue) {
+    let result = 'invalid';
+    const unitMapper = {
+        'kelvin-celsius': kelvinCelsiusConversion,
+        'kelvin-fahrenheit': kelvinFahrenheitConversion,
+        'kelvin-rankine': kelvinRankineConversion,
+        'celsius-kelvin': CelsiuskelvinConversion,
+        'celsius-fahrenheit': CelsiusCelsiusConversion,
+        'celsius-rankine': CelsiusRankineConversion,
+        'fahrenheit-kelvin': FahrenheitkelvinConversion,
+        'fahrenheit-celsius': FarhenheitCelsiusConversion,
+        'fahrenheit-rankine': FahrenheitRankineConversion,
+        'rankine-kelvin': RankineKelvinConversion,
+        'rankine-fahrenheit': RankineFarhenheitConversion,
+        'rankine-celsius': RankineCelsiusConversion,
+
+        "liters-tablespoons": LitersTablespoonsConversion,
+        "liters-cubic-inches": LiterscubicInchesConversion,
+        "liters-cups": LitersCupsConversion,
+        "liters-cubic-feet": LitersCubicFeetConversion,
+        "liters-gallons": LitersGallonsConversion,
+
+        "tablespoons-liters": TablespoonsLitersConversion,
+        "tablespoons-cubic-inches": TablespoonsCubicinchesConversion,
+        "tablespoons-cups": TablespoonsCupsConversion,
+        "tablespoons-cubic-feet": TablespoonsCubicfeetConversion,
+        "tablespoons-gallons": TablespoonsGallonsConversion,
+
+        "cubic-inches-tablespoons": CubicinchesTablespoonsConversion,
+        "cubic-inches-liters": CubicinchesLitersConversion,
+        "cubic-inches-cups": CubicinchesCupsConversion,
+        "cubic-inches-cubic-feet": CubicinchesCubicfeet,
+        "cubic-inches-gallons": CubicinchesGallons,
+
+        "cups-tablespoons": CupsTablespoonsConversion,
+        "cups-liters": CupsLitersConversion,
+        "cups-cubic-inches": CupscubicInchesConversion,
+        "cups-cubic-feet": CupsCubicfeetConversion,
+        "cups-gallons": CupsGallonsConversion,
+
+        "cubic-feet-tablespoons": CubicfeetTablespoonsConversion,
+        "cubic-feet-cubic-inches": CubicfeetCubicinchesConversion,
+        "cubic-feet-cups": CubicfeetCupsConversion,
+        "cubic-feet-liters": CubicfeetLitersConversion,
+        "cubic-feet-gallons": Cubicfeetgallons,
+
+        "gallons-tablespoons": GallonsTablespoonsConversion,
+        "gallons-cubic-inches": GallonsCubicinchesConversion,
+        "gallons-cups": GallonsCupsConversion,
+        "gallons-cubic-feet": GallonsCubicfeetConversion,
+        "gallons-liters": GallonsLitersConversion,
+    };
+    const mapperKey = sourceUnit.toLowerCase()+'-'+targetUnit.toLowerCase();
+    if(unitMapper[mapperKey]) {
+        result = unitMapper[mapperKey](sourceValue);
+    }
+    return result;
+}
+
+function kelvinCelsiusConversion(kelvin) {
+    return  kelvin - 273.15;
+}
+
+function kelvinFahrenheitConversion(kelvin) {
+    return 1.8*(kelvin - 273) + 32
+}
+
+function kelvinRankineConversion(kelvin) {
+    return kelvin *  (9/5);
+}
+
+function CelsiuskelvinConversion(celsius) {
+    return  celsius + 273.15;
+}
+
+function CelsiusCelsiusConversion(celsius) {
+    return  celsius;
+}
+
+function CelsiusRankineConversion(celsius) {
+    return  celsius * 9/5 + 491.67;
+}
+
+function FahrenheitkelvinConversion(farhn) {
+    let temp = farhn - 32;
+    return  temp * 5/9 + 273.15
+}
+
+function FarhenheitCelsiusConversion(farhn) {
+    let temp = farhn - 32;
+    return  temp * 5/9
+}
+
+function FahrenheitRankineConversion(farhn) {
+    return  farhn + 459.67
+}
+
+function RankineKelvinConversion(rakine) {
+    return  rakine * 5/9
+}
+
+function RankineFarhenheitConversion(rakine) {
+    return  rakine - 459.67
+}
+
+function RankineCelsiusConversion(rakine) {
+    let temp = rakine - 491.67
+    return  temp * 5/9
+}
+
+function LitersTablespoonsConversion(liters) {
+    return  liters * 67.628
+}
+
+function LiterscubicInchesConversion(liters) {
+    return  liters * 61.024
+}
+
+function LitersCupsConversion(liters) {
+    return  liters * 4.227
+}
+
+function LitersCubicFeetConversion(liters) {
+    return  liters / 28.317
+}
+
+function LitersGallonsConversion(liters) {
+    return  liters / 3.785
+}
+
+function TablespoonsLitersConversion(tablespoons) {
+    return  tablespoons / 256
+}
+
+function TablespoonsCubicinchesConversion(tablespoons) {
+    return  tablespoons / 1.108
+}
+
+function TablespoonsCupsConversion(tablespoons) {
+    return  tablespoons / 16
+}
+
+function TablespoonsCubicfeetConversion(tablespoons) {
+    return  tablespoons / 1915
+}
+
+function TablespoonsGallonsConversion(tablespoons) {
+    return  tablespoons / 256
+}
+
+
+function CubicinchesTablespoonsConversion(cubin) {
+    return  cubin * 1.108
+}
+
+function CubicinchesLitersConversion(cubin) {
+    return  cubin / 61.024
+}
+
+function CubicinchesCupsConversion(cubin) {
+    return  cubin / 14.438
+}
+
+function CubicinchesCubicfeet(cubin) {
+    return  cubin / 1728
+}
+
+function CubicinchesGallons(cubin) {
+    return  cubin / 231
+}
+
+function CupsTablespoonsConversion(cups) {
+    return  cups * 16
+}
+
+function CupsLitersConversion(cups) {
+    return  cups / 4.227
+}
+
+function CupscubicInchesConversion(cups) {
+    return  cups * 14.438
+}
+
+function CupsCubicfeetConversion(cups) {
+    return  cups / 120
+}
+
+function CupsGallonsConversion(cups) {
+    return  cups / 16
+}
+
+function CubicfeetTablespoonsConversion(cubft) {
+    return  cubft * 1915
+}
+
+function CubicfeetCubicinchesConversion(cubft) {
+    return  cubft * 1728
+}
+
+function CubicfeetCupsConversion(cubft) {
+    return  cubft * 120
+}
+
+function CubicfeetLitersConversion(cubft) {
+    return  cubft * 28.317
+}
+
+function Cubicfeetgallons(cubft) {
+    return  cubft * 7.481
+}
+
+function GallonsTablespoonsConversion(gallons) {
+    return  gallons * 256
+}
+
+function GallonsCubicinchesConversion(gallons) {
+    return  gallons * 231
+}
+
+function GallonsCupsConversion(gallons) {
+    return  gallons * 16
+}
+
+function GallonsCubicfeetConversion(gallons) {
+    return  gallons / 7.481
+}
+
+function GallonsLitersConversion(gallons) {
+    return  gallons * 3.785
+}
+
+export {convertUnits};
